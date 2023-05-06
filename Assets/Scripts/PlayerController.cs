6063b613a12518b635c3cca750e41f4ceae9a339
Braian Yacante <byacante@i-rent.net>
2023-05-05 21:18:55 -0300

    create jump function

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float jumpSpeed = 5;
+    private Rigidbody rb;
+    private void Jump()
+    {
+        rb.AddForce(0, jumpSpeed, 0, ForceMode.Impulse);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
-        
+
     // Update is called once per frame
     void FixedUpdate()
     {
-        
+
     }
 }
